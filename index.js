22789f380552c0a9b673b59f24d391d05eeadee7
Kaiser-G <kaiserg6969@gmail.com>
2021-05-30 16:16:00 +0200

    un peu de test

---
+++ index.js
@@ -0,0 +1,9 @@
+let string = "je suis une chaine";
+let number = 24;
+let boolean = true;
+let array = ["je", "suis", 24, false];
+let object = {
+  prenom: "audrey",
+  age: 34,
+  ville: "bordeaux",
+};
